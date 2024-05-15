22c806d7ff68ce40bd4bb96da3bf2ee716fd90e2
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-05-15 22:26:15 +0300

    update header

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,17 +1,65 @@
-import { useSelector } from 'react-redux';
-import Navigation from '../Navigation/Navigation';
-import AuthNav from '../AuthNav/AuthNav';
-import UserMenu from '../UserMenu/UserMenu';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+
 import { selectIsLoggedIn } from '../../redux/auth/selectors';
-import css from './AppBar.module.css';
+import { selectUser } from '../../redux/auth/selectors';
+import { logout } from '../../redux/auth/operations';
+import clsx from 'clsx';
+
+import css from '../Navigation/Navigation.module.css';
+import css3 from '../AppBar/AppBar.module.css';
+import css1 from '../UserMenu/UserMenu.module.css';
+import css2 from '../AuthNav/AuthNav.module.css';
+
+const getNavLinkClassName = ({ isActive }) =>
+  clsx(css.navLink, {
+    [css.active]: isActive,
+  });
 
 export const AppBar = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
 
   return (
-    <header className={css.header}>
-      <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    <header className={css3.header}>
+      {/* <Navigation /> */}
+      <nav>
+        <NavLink to="/" className={getNavLinkClassName}>
+          Home
+        </NavLink>
+        {isLoggedIn && (
+          <NavLink to="/contacts" className={getNavLinkClassName}>
+            Contacts
+          </NavLink>
+        )}
+      </nav>
+      {/* {isLoggedIn ? <UserMenu /> : <AuthNav />} */}
+      {isLoggedIn && (
+        <div className={css1.wrapper}>
+          <p className={css1.welcome}>
+            Welcome <span className={css1.username}>{user.name}</span>
+          </p>
+          <button
+            type="button"
+            className={css1.btn}
+            onClick={() => dispatch(logout())}
+          >
+            Logout
+          </button>
+        </div>
+      )}
+      {!isLoggedIn && (
+        <div className={css2.registrationLoginWrap}>
+          <NavLink to="/register" className={getNavLinkClassName}>
+            Registration
+          </NavLink>
+
+          <NavLink to="/login" className={getNavLinkClassName}>
+            Login
+          </NavLink>
+        </div>
+      )}
     </header>
   );
 };
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,26 +1,26 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { selectUser } from '../../redux/auth/selectors';
-import { logout } from '../../redux/auth/operations';
-import css from './UserMenu.module.css';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { selectUser } from '../../redux/auth/selectors';
+// import { logout } from '../../redux/auth/operations';
+// import css from './UserMenu.module.css';
 
-export const UserMenu = () => {
-  const dispatch = useDispatch();
-  const user = useSelector(selectUser);
+// export const UserMenu = () => {
+//   const dispatch = useDispatch();
+//   const user = useSelector(selectUser);
 
-  return (
-    <div className={css.wrapper}>
-      <p className={css.welcome}>
-        Welcome <span className={css.username}>{user.name}</span>
-      </p>
-      <button
-        type="button"
-        className={css.btn}
-        onClick={() => dispatch(logout())}
-      >
-        Logout
-      </button>
-    </div>
-  );
-};
+//   return (
+//     <div className={css.wrapper}>
+//       <p className={css.welcome}>
+//         Welcome <span className={css.username}>{user.name}</span>
+//       </p>
+//       <button
+//         type="button"
+//         className={css.btn}
+//         onClick={() => dispatch(logout())}
+//       >
+//         Logout
+//       </button>
+//     </div>
+//   );
+// };
 
-export default UserMenu;
+// export default UserMenu;

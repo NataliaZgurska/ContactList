0b91a4c46cedd33f2ae2a95bd74893a8b2ffff37
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-05-24 21:20:58 +0300

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,60 +1,38 @@
 import { Helmet } from 'react-helmet-async';
 import css from './HomePage.module.css';
-import firstPicture from './tulips-min.jpg';
+import { useEffect, useState } from 'react';
+import { getImages } from '../../services/api';
+import ImageGallery from '../../components/ImageGallery/ImageGallery';
 
 const HomePage = () => {
+  const [images, setImages] = useState(null);
+
+  const fetchImages = async () => {
+    try {
+      const data = await getImages();
+      setImages(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
   return (
     <>
       <Helmet>
         <title>HomePage</title>
       </Helmet>
       <div className={css.homePageContainer}>
-        <h1 className={css.homePageTitle}>Будь на зв'язку</h1>
-        <ul className={css.homePagePicturesList}>
-          <li className={css.homePagePiccturesItem}>
-            <div className={css.HomePageImgWrap}>
-              <img
-                src={firstPicture}
-                alt="tulips"
-                className={css.homePageImg}
-              />
-            </div>
-          </li>
-          <li className={css.homePagePiccturesItem}>
-            <div className={css.HomePageImgWrap}>
-              <img
-                src={firstPicture}
-                alt="tulips"
-                className={css.homePageImg}
-              />
-            </div>
-          </li>
-          <li className={css.homePagePiccturesItem}>
-            <div className={css.HomePageImgWrap}>
-              <img
-                src={firstPicture}
-                alt="tulips"
-                className={css.homePageImg}
-              />
-            </div>
-          </li>
-          <li className={css.homePagePiccturesItem}>
-            <div className={css.HomePageImgWrap}>
-              <img
-                src={firstPicture}
-                alt="tulips"
-                className={css.homePageImg}
-              />
-            </div>
-          </li>
-        </ul>
-        <p className={css.welcomeText}>
+        <h1 className={css.homePageTitle}>
           Welcome! Save and manage your contacts
-        </p>
-        <footer className={css.footerText}>
-          "Ну що б, здавалося, слова... Слова та голос - більш нічого. А серце
-          б'ється - ожива, як їх почує" Т.Шевченко
-        </footer>
+        </h1>
+
+        <ImageGallery images={images} />
+
+        <footer className={css.footerText}>Stay in touch!</footer>
       </div>
     </>
   );
